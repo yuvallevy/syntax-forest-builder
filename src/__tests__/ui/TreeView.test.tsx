f906e51dc8173b56472e0a15f07b7b2017320697
Yuval Levy <ylevy12@gmail.com>
2022-11-24 14:39:23 +0200

    Add SVG-based tree display

---
+++ src/__tests__/ui/TreeView.test.tsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+import { PositionedTree } from '../../core/types';
+import TreeView from '../../ui/TreeView';
+
+const zeroOffset = { offset: { dTreeX: 0, dTreeY: 0 } };
+
+describe('tree rendering', () => {
+  const tree: PositionedTree = {
+    sentence: 'Noun verbs very adverbly.',
+    nodes: {
+      'a': { label: 'S', position: { treeX: 53.625, treeY: -80 }, children: {
+        'b': { label: 'NP', position: { treeX: 18, treeY: -60 }, children: {
+          'c': { label: 'N', position: { treeX: 18, treeY: -2 } },
+        } },
+        'd': { label: 'VP', position: { treeX: 89.25, treeY: -60 }, children: {
+          'e': { label: 'V', position: { treeX: 57, treeY: -2 } },
+          'f': { label: 'AdvP', position: { treeX: 121.5, treeY: -30 }, triangle: { treeX1: 72, treeX2: 104 } },
+        } },
+      } },
+    },
+    position: { plotX: 50, plotY: -32 },
+  };
+
+  it('renders a tree with position-assigned nodes', () => {
+    expect(render(
+      <svg>  {/** Render inside an SVG tag to prevent "unrecognized tag" errors in console */}
+        <TreeView treeId="tree1" tree={tree} />
+      </svg>
+    ).asFragment()).toMatchSnapshot();
+  });
+});
---
+++ src/ui/TreeView.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { mapEntries } from '../core/objTransforms';
+import { Id, PositionedBranchingNode, PositionedNode, PositionedTerminalNode, PositionedTree } from '../core/types';
+
+const NODE_LEVEL_SPACING = 20;
+const TRIANGLE_BASE_Y = -2;
+
+interface TreeViewProps {
+  treeId: Id;
+  tree: PositionedTree;
+}
+
+const renderChildNodeConnections = (node: PositionedBranchingNode): React.ReactNode[] =>
+  mapEntries(node.children, ([childId, child]) =>
+    <line
+      key={`to-${childId}`}
+      stroke="#000"
+      x1={node.position.treeX}
+      y1={node.position.treeY}
+      x2={child.position.treeX}
+      y2={child.position.treeY - NODE_LEVEL_SPACING}
+    />
+  );
+
+const renderTriangleConnection = (nodeId: Id, node: PositionedTerminalNode): React.ReactNode =>
+  node.triangle && <path
+    key={`triangle-${nodeId}`}
+    stroke="#000"
+    fill="none"
+    d={`M${node.position.treeX} ${node.position.treeY} L${node.triangle.treeX1} ${TRIANGLE_BASE_Y}  L${node.triangle.treeX2} ${TRIANGLE_BASE_Y} Z`}
+  />;
+
+const renderNodeFlat = ([nodeId, node]: [Id, PositionedNode]): React.ReactNode[] => [
+  <text
+    key={nodeId}
+    x={node.position.treeX}
+    y={node.position.treeY}
+    fill="#000"
+    textAnchor="middle"
+    dominantBaseline="text-after-edge"
+  >
+    {node.label}
+  </text>,
+  'triangle' in node && renderTriangleConnection(nodeId, node),
+  ...('children' in node)
+    ? [
+      ...renderChildNodeConnections(node),
+      ...mapEntries(node.children, renderNodeFlat)
+    ] : [],
+];
+
+const TreeView: React.FC<TreeViewProps> = ({ treeId, tree }) =>
+  <g id={`tree-${treeId}`} style={{ transform: `translate(${tree.position.plotX}px, ${tree.position.plotY}px)` }}>
+    {mapEntries(tree.nodes, renderNodeFlat)}
+  </g>;
+
+export default TreeView;
