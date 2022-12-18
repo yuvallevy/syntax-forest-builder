a09f62863cf2d0e56ccf421e47c588413d7844b4
Yuval Levy <ylevy12@gmail.com>
2022-12-18 15:51:10 +0200

    View test of a sample tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { applyNodePositionsToPlot } from './core/positioning';
+import { PositionedPlot, UnpositionedPlot } from './core/types';
+import PlotView from './ui/PlotView';
+import mockStrWidth from './__tests__/__mocks__/mockStrWidth';
 
-function App() {
+const testUnpositionedPlot: UnpositionedPlot = {
+  trees: {
+    'aa': {
+      sentence: 'Noun verbs very adverbly.',
+      nodes: {
+        'a': { label: 'S', offset: { dTreeX: 0, dTreeY: 0 }, children: {
+          'b': { label: 'NP', offset: { dTreeX: 0, dTreeY: -10 }, children: {
+            'c': { label: 'N', offset: { dTreeX: 0, dTreeY: 0 }, slice: [0, 4], triangle: false },
+          } },
+          'd': { label: 'VP', offset: { dTreeX: 0, dTreeY: 0 }, children: {
+            'e': { label: 'V', offset: { dTreeX: 0, dTreeY: 0 }, slice: [5, 10], triangle: false },
+            'f': { label: 'AdvP', offset: { dTreeX: 0, dTreeY: 0 }, slice: [11, 24], triangle: true },
+          } },
+        } },
+      },
+      offset: { dPlotX: 200, dPlotY: 150 },
+    },
+  },
+};
+
+const testPositionedPlot: PositionedPlot = applyNodePositionsToPlot(mockStrWidth)(testUnpositionedPlot);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PlotView plot={testPositionedPlot} />
   );
 }
 
