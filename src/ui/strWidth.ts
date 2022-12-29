e8668ebe54e44780fd87b78b8f2d1637084db286
Yuval Levy <ylevy12@gmail.com>
2022-12-29 16:19:28 +0200

    Placeholder for future string width function

---
+++ src/ui/strWidth.ts
@@ -0,0 +1,6 @@
+import mockStrWidth from '../__tests__/__mocks__/mockStrWidth';
+
+// Overwrite this with an actual string width measuring function when the time comes
+const strWidth = mockStrWidth;
+
+export default strWidth;
