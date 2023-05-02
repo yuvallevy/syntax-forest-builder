16870eb3165b8c9ae67958476f5d72fbc4f41cf2
Yuval Levy <ylevy12@gmail.com>
2023-05-02 19:53:26 +0300

    Remove starting tree

--- src/ui/contentState.ts
+++ src/ui/contentState.ts
@@ -176,20 +176,7 @@ export type UndoableContentState = UndoRedoHistory<UndoableContentChange, Conten
 const initialState: ContentState = {
   plots: {
     'plot': {
-      trees: {
-        'aa': {
-          sentence: 'Noun verbs very adverbly.',
-          nodes: {
-            // 'a': { label: 'S', offset: { dTreeX: 0, dTreeY: 0 }, children: ['b', 'd'] },
-            'b': { label: 'NP', offset: { dTreeX: 0, dTreeY: -10 }, children: ['c'] },
-            'c': { label: 'N', offset: { dTreeX: 0, dTreeY: 0 }, slice: [0, 4], triangle: false },
-            'd': { label: 'VP', offset: { dTreeX: 0, dTreeY: 0 }, children: ['e', 'f'], },
-            'e': { label: 'V', offset: { dTreeX: 0, dTreeY: 0 }, slice: [5, 10], triangle: false },
-            'f': { label: 'AdvP', offset: { dTreeX: 0, dTreeY: 0 }, slice: [11, 24], triangle: true },
-          },
-          offset: { dPlotX: 200, dPlotY: 250 },
-        },
-      },
+      trees: {},
     },
   },
 };
