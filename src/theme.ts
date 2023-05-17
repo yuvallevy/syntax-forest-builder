feaa77f42fc98b89c72634d32525796022d9063a
Yuval Levy <ylevy12@gmail.com>
2023-05-17 23:45:26 +0300

    Use web font

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,8 @@ const nextToPrimaryColor = 'rgba(20, 177, 163)';
 const theme: MantineThemeOverride = {
   defaultGradient: { deg: 60, from: primaryColor, to: nextToPrimaryColor },
   primaryColor,
+  fontFamily: '"Wix Madefor Text",-apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Helvetica",' +
+    '"Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif',
 };
 
 export default theme;
