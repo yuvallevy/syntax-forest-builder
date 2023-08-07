7b0d215a654072116ebd6286a4edc9532ddbca2d
Yuval Levy <ylevy12@gmail.com>
2023-08-08 01:44:52 +0300

    Initial Kotlin-side tests

---
+++ npbloom-core/src/commonTest/kotlin/content/unpositioned/PlotCoordsOffsetTest.kt
@@ -0,0 +1,12 @@
+package content.unpositioned
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PlotCoordsOffsetTest {
+    @Test
+    fun plotCoordsOffsetAddition() = assertEquals(
+        PlotCoordsOffset(5.0, 15.3),
+        PlotCoordsOffset(4.0, 19.0) + PlotCoordsOffset(1.0, -3.7)
+    )
+}
