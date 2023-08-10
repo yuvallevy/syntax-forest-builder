edb4a2c6d6c4dae2bbf4e9d0fe98e8574a89a64c
Yuval Levy <ylevy12@gmail.com>
2023-08-10 18:41:34 +0300

    Translate history mechanism and tests

---
+++ npbloom-core/src/commonTest/kotlin/history/testActionAndState.kt
@@ -0,0 +1,26 @@
+package history
+
+data class TestState(
+    val numberValue: Int,
+    val stringValue: String,
+)
+
+sealed interface TestAction : UndoableActionCommon
+
+data class IncrementNumber(val difference: Int) : TestAction
+data class DecrementNumber(val difference: Int) : TestAction
+data class SetString(val oldValue: String, val newValue: String) : TestAction
+
+fun testApplyAction(state: TestState, action: TestAction) =
+    when (action) {
+        is DecrementNumber -> state.copy(numberValue = state.numberValue - action.difference)
+        is IncrementNumber -> state.copy(numberValue = state.numberValue + action.difference)
+        is SetString -> state.copy(stringValue = action.newValue)
+    }
+
+fun testReverseAction(action: TestAction) =
+    when (action) {
+        is DecrementNumber -> IncrementNumber(action.difference)
+        is IncrementNumber -> DecrementNumber(action.difference)
+        is SetString -> action.copy(oldValue = action.newValue, newValue = action.oldValue)
+    }
