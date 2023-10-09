5a9238d5c21ac9e4052c44202a80d2695bf46705
Yuval Levy <ylevy12@gmail.com>
2023-10-10 00:19:03 +0300

    Use platform-specific functions for timestamps

--- npbloom-core/src/commonMain/kotlin/ui/content/GenerateId.kt
+++ npbloom-core/src/commonMain/kotlin/ui/content/GenerateId.kt
@@ -2,11 +2,10 @@
 
 package ui.content
 
-import kotlin.js.Date
-import kotlin.random.Random
-import kotlin.random.nextLong
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
+import kotlin.random.Random
+import kotlin.random.nextLong
 
 private const val ID_TIME_LENGTH = 6
 private const val ID_RANDOM_LENGTH = 4
@@ -15,9 +14,11 @@ private const val ID_BASE = 36
 private val maxTimestamp = ("1" + "0".repeat(ID_TIME_LENGTH)).toLong(ID_BASE)
 private val maxRandom = ("1" + "0".repeat(ID_RANDOM_LENGTH)).toLong(ID_BASE)
 
+internal expect fun timeAsNumber(): Long
+
 private fun generateId(prefix: String) =
     prefix +
-            (Date().getTime().toInt() % maxTimestamp).toString(ID_BASE).padStart(ID_TIME_LENGTH, '0') +
+            (timeAsNumber() % maxTimestamp).toString(ID_BASE).padStart(ID_TIME_LENGTH, '0') +
             Random.nextLong(0 until maxRandom).toString(ID_BASE).padStart(ID_RANDOM_LENGTH, '0')
 
 @JsExport
