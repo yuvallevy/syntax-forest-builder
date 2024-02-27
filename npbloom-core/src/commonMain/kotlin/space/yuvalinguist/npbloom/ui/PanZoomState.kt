404a8428caad5a79e38ddeeed88865175610e578
Yuval Levy <ylevy12@gmail.com>
2024-02-27 00:04:50 -0500

    Clamp zoom level to 10%..1000% range

--- npbloom-core/src/commonMain/kotlin/space/yuvalinguist/npbloom/ui/PanZoomState.kt
+++ npbloom-core/src/commonMain/kotlin/space/yuvalinguist/npbloom/ui/PanZoomState.kt
@@ -10,12 +10,16 @@ typealias ZoomLevel = Double
 
 @JsExport
 data class PanZoomState(val viewPositionInPlot: PlotCoordsOffset, val zoomLevel: ZoomLevel) {
+    private val allowedZoomRange = 0.1..10.0
+
     fun panBy(relativePanOffset: ClientCoordsOffset) =
         copy(viewPositionInPlot = viewPositionInPlot - relativePanOffset.toPlotCoordsOffset(this))
 
     fun zoomBy(relativeFactor: Double, focusInClient: ClientCoordsOffset): PanZoomState {
         val focusInPlot = focusInClient.toPlotCoordsOffset(this)
         val newZoomLevel = zoomLevel * relativeFactor
+        if (newZoomLevel !in allowedZoomRange)
+            return setZoomLevel(newZoomLevel.coerceIn(allowedZoomRange), focusInClient)
         val newViewPositionInPlot = viewPositionInPlot + focusInPlot - focusInPlot / relativeFactor
         return copy(viewPositionInPlot = newViewPositionInPlot, zoomLevel = newZoomLevel)
     }
