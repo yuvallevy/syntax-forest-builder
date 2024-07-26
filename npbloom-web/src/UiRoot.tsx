600c3b7651081b7ac61be5b3845733ea0ca06bd7
Yuval Levy <ylevy12@gmail.com>
2024-07-25 23:46:53 -0400

    Extract menu to its own component, providing immense relief

--- npbloom-web/src/UiRoot.tsx
+++ npbloom-web/src/UiRoot.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import { AppShell, Button, Flex, Header, MantineProvider, Menu, Space, Text } from '@mantine/core';
-import { IconDeviceFloppy, IconFolder } from '@tabler/icons-react';
+import { AppShell, MantineProvider } from '@mantine/core';
 import theme from './theme';
 import './App.scss';
 import { Id } from './types';
@@ -11,29 +10,21 @@ import {
 } from 'npbloom-core';
 import PlotView from './components/PlotView';
 import useHotkeys from '@reecelucas/react-use-hotkeys';
-import Settings from './components/meta/Settings';
+import MainMenu from './components/MainMenu';
 import Toolbox from './components/Toolbox';
-import AboutButton from './components/meta/AboutButton';
 import NewVersionModal from './components/meta/NewVersionModal.tsx';
 import PlotSelector from './components/PlotSelector';
 import BeginnersGuide from './components/meta/BeginnersGuide';
 import PlotPlaceholder from './components/meta/PlotPlaceholder';
 import useUiState from './useUiState';
-import useFileIo from './io/useFileIo';
-import { useOs } from '@mantine/hooks';
-import substituteOsAwareHotkey from './components/substituteOsAwareHotkey';
 import useHeldHotkey from './useHeldHotkey';
-import { MAIN_MENU_HEIGHT } from './uiDimensions.ts';
 import './UiRoot.scss';
 
 const UiRoot = () => {
   const { state, dispatch } = useUiState();
   const { selection, activePlotIndex } = state;
 
-  const os = useOs();
-
   const [beginnersGuideActive, setBeginnersGuideActive] = useState<boolean>(false);
-  const { fileIoModalComponent, activeFileName, openFileSaveModal, openFileLoadModal, saveOrSaveAs } = useFileIo();
 
   const selectedNodeIndicators = selection instanceof NodeSelectionInPlot ? selection.nodeIndicatorsAsArray : [];
 
@@ -89,12 +80,6 @@ const UiRoot = () => {
   useHotkeys(['Control+y', 'Meta+y'], event => { event.preventDefault(); redo(); },
     { ignoredElementWhitelist: ['INPUT'] });
 
-  useHotkeys(['Control+o', 'Meta+o'], event => { event.preventDefault(); openFileLoadModal(); },
-    { ignoredElementWhitelist: ['INPUT'] });
-
-  useHotkeys(['Control+s', 'Meta+s'], event => { event.preventDefault(); saveOrSaveAs(); },
-    { ignoredElementWhitelist: ['INPUT'] });
-
   useHotkeys(Array.from('abcdefghijklmnopqrstuvwxyz', letter => `Shift+${letter}`), () => {
     if (state.selection instanceof NodeSelectionInPlot && state.selection.nodeIndicatorsAsArray.length === 1) {
       startEditing();  // For some reason the key press passes right through to the newly-created input.
@@ -108,47 +93,9 @@ const UiRoot = () => {
         && dispatch(new SetSelectionAction(EntitySelectionAction.SelectNode))
   );
 
-  const mainMenu = <Header height={MAIN_MENU_HEIGHT}>
-    <Flex align="stretch" className="MainMenu">
-      <Menu shadow="md" offset={0} position="top-start" transitionProps={{ transition: 'scale-y' }} width={'18ch'}>
-        <Menu.Target>
-          <Button variant="subtle" size="sm">
-            File
-          </Button>
-        </Menu.Target>
-        <Menu.Dropdown>
-          <Menu.Item
-            icon={<IconFolder size={14} />}
-            rightSection={<Text color="dimmed">{substituteOsAwareHotkey('Ctrl-O', os)}</Text>}
-            onClick={openFileLoadModal}
-          >
-            Open...
-          </Menu.Item>
-          <Menu.Item
-            icon={<IconDeviceFloppy size={14} />}
-            rightSection={<Text color="dimmed">{substituteOsAwareHotkey('Ctrl-S', os)}</Text>}
-            onClick={saveOrSaveAs}
-          >
-            Save{activeFileName ? '' : '...'}
-          </Menu.Item>
-          <Menu.Item disabled={!activeFileName} onClick={openFileSaveModal}>
-            Save As...
-          </Menu.Item>
-          {activeFileName && <><Menu.Divider />
-          <Menu.Item disabled>
-            Currently open file:<br />{activeFileName}
-          </Menu.Item></>}
-        </Menu.Dropdown>
-      </Menu>
-      <Space style={{ flexGrow: 1 }} />
-      <Settings />
-      <AboutButton />
-    </Flex>
-  </Header>;
-
   return <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
     <AppShell
-      header={mainMenu}
+      header={<MainMenu />}
       navbar={<Toolbox />}
       footer={<PlotSelector />}
       padding={0}
@@ -161,7 +108,6 @@ const UiRoot = () => {
       showWelcome={!state.contentState.canUndo && !state.contentState.canRedo}
       onDemoRequest={() => setBeginnersGuideActive(true)}
     />}
-    {fileIoModalComponent}
     <NewVersionModal />
   </MantineProvider>;
 }
---
+++ npbloom-web/src/components/MainMenu.tsx
@@ -0,0 +1,98 @@
+import { Fragment } from 'react';
+import { Button, Flex, Header, Menu, Space, Text } from '@mantine/core';
+import { useOs } from '@mantine/hooks';
+import { IconDeviceFloppy, IconFolder } from '@tabler/icons-react';
+import { MAIN_MENU_HEIGHT } from '../uiDimensions.ts';
+import useHotkeys from '@reecelucas/react-use-hotkeys';
+import substituteOsAwareHotkey from './substituteOsAwareHotkey.ts';
+import Settings from './meta/Settings';
+import AboutButton from './meta/AboutButton';
+import useFileIo from '../io/useFileIo';
+
+type MenuItem = {
+  label: string;
+  action?: (() => void) | (() => Promise<void>);
+  icon?: JSX.Element;
+  hotkey?: string;
+  disabled?: boolean;
+  hidden?: boolean;
+};
+
+type MenuItemGroup = MenuItem[];
+
+type MenuSection = MenuItemGroup[];
+
+type NamedMenuSection = [string, MenuSection];
+
+const MainMenu: React.FC = () => {
+  const { fileIoModalComponent, activeFileName, openFileSaveModal, openFileLoadModal, saveOrSaveAs } = useFileIo();
+
+  const os = useOs();
+
+  useHotkeys(['Control+o', 'Meta+o'], event => { event.preventDefault(); openFileLoadModal(); },
+    { ignoredElementWhitelist: ['INPUT'] });
+
+  useHotkeys(['Control+s', 'Meta+s'], event => { event.preventDefault(); saveOrSaveAs(); },
+    { ignoredElementWhitelist: ['INPUT'] });
+
+  const mainMenuElements: NamedMenuSection[] = [
+    ['File', [
+      [
+        { label: 'Open...', icon: <IconFolder size={14}/>, hotkey: 'Ctrl-O', action: openFileLoadModal },
+        { label: activeFileName ? 'Save' : 'Save...', icon: <IconDeviceFloppy size={14}/>, hotkey: 'Ctrl-S',
+          action: saveOrSaveAs },
+        { label: 'Save As...', disabled: !activeFileName, action: openFileSaveModal },
+      ],
+      [
+        { label: activeFileName ? `Currently open file:\n${activeFileName}` : '', disabled: true, hidden: !activeFileName },
+      ],
+    ]],
+  ]
+
+  return <>
+    <Header height={MAIN_MENU_HEIGHT}>
+      <Flex align="stretch" className="MainMenu">
+        {mainMenuElements.map(([sectionName, sectionGroups]) => (
+          <Menu
+            key={sectionName}
+            shadow="md"
+            offset={0}
+            position="top-start"
+            transitionProps={{ transition: 'scale-y' }}
+            width={'18ch'}
+          >
+            <Menu.Target>
+              <Button variant="subtle" size="sm">
+                {sectionName}
+              </Button>
+            </Menu.Target>
+            <Menu.Dropdown>
+              {sectionGroups.map((groupItems, groupIndex) => <Fragment key={groupIndex}>
+                {groupItems.map((item, itemIndex) =>
+                  item.hidden ? null : <Menu.Item
+                    key={itemIndex}
+                    icon={item.icon}
+                    rightSection={item.hotkey && <Text color="dimmed">{substituteOsAwareHotkey(item.hotkey, os)}</Text>}
+                    style={{ whiteSpace: 'pre-line' }}
+                    disabled={item.disabled}
+                    onClick={item.action}
+                  >
+                    {item.label}
+                  </Menu.Item>)}
+                {groupIndex < sectionGroups.length - 1
+                  && sectionGroups[groupIndex + 1].some(({ hidden }) => !hidden)
+                  && <Menu.Divider />}
+              </Fragment>)}
+            </Menu.Dropdown>
+          </Menu>
+        ))}
+        <Space style={{ flexGrow: 1 }} />
+        <Settings />
+        <AboutButton />
+      </Flex>
+    </Header>
+    {fileIoModalComponent}
+  </>;
+};
+
+export default MainMenu;
