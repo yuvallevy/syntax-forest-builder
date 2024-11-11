51636c805ec473198c9c7d43389f4d36d9a3bd92
Yuval Levy <ylevy12@gmail.com>
2024-11-11 17:03:07 -0500

    Import/export functionality

--- npbloom-web/.eslintrc.cjs
+++ npbloom-web/.eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
     'react-refresh/only-export-components': 'warn',
     'no-prototype-builtins': 'off',
     'no-unused-vars': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-unused-vars': [
