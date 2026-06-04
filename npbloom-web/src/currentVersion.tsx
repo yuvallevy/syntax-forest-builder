0f2b8e5789dbf86d425d08648748384717624fe3
Yuval Levy <ylevy12@gmail.com>
2026-06-04 13:22:58 -0400

    Add example changes for new version modal

--- npbloom-web/src/currentVersion.tsx
+++ npbloom-web/src/currentVersion.tsx
@@ -15,34 +15,18 @@ type Change = { icon: (...args: any[]) => JSX.Element; title: string; badge?: Ch
 // since it will be shown to users who may not be familiar with the internal workings of the app.
 export const changesFromPreviousVersion: Change[] = [
 //   {
-//     icon: IconCircleSquare,
-//     title: 'Shapes',
-//     badge: 'New',
-//     description: 'Draw simple shapes directly on the canvas to annotate and mark up your trees.',
+//     icon: IconExample1,
+//     title: 'Brief title',
+//     badge: 'New/Improved/Fix',
+//     description: 'More detailed description of the change as plain text.',
 //   },
 //   {
-//     icon: IconShare2,
-//     title: 'Forest import & export',
-//     badge: 'New',
+//     icon: IconExample2,
+//     title: 'Another change',
+//     badge: 'New/Improved/Fix',
 //     description: <>
-//       Transfer entire forests between devices or share them with colleagues or students using the new{' '}
-//       <IconFileExport className="menu-icon" /> <b>Export forest...</b> and <IconFileImport className="menu-icon" /> <b>Import forest...</b> options in the File menu.
+//       More detailed description of the change, which can include <IconExample2 className="menu-icon" /> <b>menu icons</b> or <IconExample2 className="toolbox-icon" /> toolbox icons.
 //     </>,
-//   },
-//   {
-//     icon: IconPhoto,
-//     title: 'Export tree as image',
-//     badge: 'New',
-//     description: <>
-//       Export a single tree as a PNG file.{' '}
-//       Select a tree, then select <IconPhotoShare className="menu-icon" /> <b>Export tree as image...</b> from the File menu or click <IconPhotoShare className="toolbox-icon" /> on the toolbox.
-//     </>,
-//   },
-//   {
-//     icon: IconActivity,
-//     title: 'Performance & UX',
-//     badge: 'Improved',
-//     description: 'Minor improvements to rendering performance, graphics quality, and general usability.',
 //   },
   {
     icon: IconClick,
