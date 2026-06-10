ed6a15d3afe4839824e0dcdab4c9bf3f1680c36e
Yuval Levy <ylevy12@gmail.com>
2026-06-10 14:42:14 -0400

    Recommend using a desktop or laptop

--- npbloom-web/src/components/meta/PlotPlaceholder.tsx
+++ npbloom-web/src/components/meta/PlotPlaceholder.tsx
@@ -16,8 +16,15 @@ const PlotPlaceholder: React.FC<PlotPlaceholderProps> = ({ showWelcome, acceptMo
       title="Welcome to NPBloom!"
       style={{ pointerEvents: acceptMouseEvents ? 'auto' : 'none' }}
     >
-      To start, click anywhere and type a sentence,
-      or <Anchor onClick={onTourRequest}>take the guided tour</Anchor>.
+      <div className="PlotPlaceholder--desktop-alert-content">
+        To start, click anywhere and type a sentence,
+        or <Anchor onClick={onTourRequest}>take the guided tour</Anchor>.
+      </div>
+      <div className="PlotPlaceholder--mobile-alert-content">
+        NPBloom works best on desktop.<br />
+        Come back on a larger screen to start,
+        or <Anchor onClick={onTourRequest}>take the guided tour</Anchor>.
+      </div>
     </Alert> : <img src={LogoDim} height={120} alt="NPBloom logo" />}
   </div>;
 
